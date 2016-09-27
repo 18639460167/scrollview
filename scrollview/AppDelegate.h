900d42ab309f15f93985e6653c0825fbe730bd20
tenpastnine-ios-dev <1587337963@qq.com>
2016-09-27 13:49:02 +0800

    Initial Commit

---
+++ scrollview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scrollview
+//
+//  Created by tenpastnine-ios-dev on 16/9/27.
+//  Copyright © 2016年 Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scrollview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scrollview
+//
+//  Created by tenpastnine-ios-dev on 16/9/27.
+//  Copyright © 2016年 Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
